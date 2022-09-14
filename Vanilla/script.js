f11cd88a8ea51af3bbf39601ef769a5e1d89a47c
Yuan <gaoyuan0705@gmail.com>
2022-09-13 20:34:38 -0400

    NaN stuff

--- Vanilla/script.js
+++ Vanilla/script.js
@@ -1,8 +1,12 @@
+"use strict"
+
 const slotKeys = document.querySelectorAll(".slot-keys span");
 const infoElement = document.querySelector(".info");
+const billInserted = document.querySelector(".input input");
+console.dir(billInserted);
+// console.log(slotKeys);
 
-console.log(slotKeys);
-
+// create a map that contains product infomation
 const inventoryList = new Map([
     ["A1",{
         name: "Coke",
@@ -11,47 +15,47 @@ const inventoryList = new Map([
         nv:{
             calories:10,
             sugar:5,
-            carb:0,
-            sodium:0,
+            carb:2,
+            sodium:13,
         }
     }],
     ["B1",{
         name: "Chip",
-        price:3.5,
+        price:4.5,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:7,
+            sugar:4,
+            carb:8,
+            sodium:2,
         }
     }],
     ["C1",{
         name: "Donut",
-        price:3.5,
+        price:5.5,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:28,
+            sugar:1,
+            carb:9,
+            sodium:5,
         }
     }],
     ["D1",{
         name: "Monster",
-        price:3.5,
+        price:6.5,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:38,
+            sugar:7,
+            carb:6,
+            sodium:3,
         }
     }],
 
     ["A2",{
         name: "Milk",
-        price:3.5,
+        price:1.5,
         amount:6,
         nv:{
             calories:10,
@@ -62,133 +66,195 @@ const inventoryList = new Map([
     }],
     ["B2",{
         name: "Chocolate Milk",
-        price:3.5,
+        price:4,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:110,
+            sugar:21,
+            carb:62,
+            sodium:11,
         }
     }],
     ["C2",{
         name: "Timibits",
-        price:3.5,
+        price:1,
         amount:6,
         nv:{
             calories:10,
             sugar:5,
-            carb:0,
-            sodium:0,
+            carb:3,
+            sodium:2.5,
         }
     }],
     ["D2",{
-        name: "Coke",
-        price:3.5,
+        name: "Sprite",
+        price:3,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:210,
+            sugar:54,
+            carb:20,
+            sodium:10,
         }
     }],
 
     ["A3",{
-        name: "Coke",
-        price:3.5,
+        name: "Fanta",
+        price:4,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:140,
+            sugar:18,
+            carb:9,
+            sodium:7,
         }
     }],
     ["B3",{
-        name: "Coke",
-        price:3.5,
+        name: "Pepsi",
+        price:6,
         amount:6,
         nv:{
-            calories:10,
+            calories:130,
             sugar:5,
-            carb:0,
-            sodium:0,
+            carb:8,
+            sodium:7,
         }
     }],
     ["C3",{
-        name: "Coke",
-        price:3.5,
+        name: "Skittles",
+        price:3.9,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:16,
+            sugar:15,
+            carb:17,
+            sodium:18,
         }
     }],
     ["D3",{
         name: "Candy",
-        price:3.5,
+        price:1.5,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:14,
+            sugar:25,
+            carb:20,
+            sodium:20,
         }
     }],
 
     ["A4",{
         name: "Protein bar",
-        price:3.5,
+        price:5.5,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:290,
+            sugar:13,
+            carb:19,
+            sodium:20,
         }
     }],
     ["B4",{
-        name: "Coke",
-        price:3.5,
+        name: "Coffee",
+        price:1,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
+            calories:0,
+            sugar:0,
             carb:0,
             sodium:0,
         }
     }],
     ["C4",{
-        name: "Coke",
-        price:3.5,
+        name: "Bubble Tea",
+        price:3.6,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:18,
+            sugar:17,
+            carb:98,
+            sodium:99,
         }
     }],
     ["D4",{
-        name: "Coke",
-        price:3.5,
+        name: "Apple Pie",
+        price:6.7,
         amount:6,
         nv:{
-            calories:10,
-            sugar:5,
-            carb:0,
-            sodium:0,
+            calories:37,
+            sugar:45,
+            carb:99,
+            sodium:36,
         }
     }],
 ]);
 
+// get nutrition value and show up on the left form 
+function fillNv(fact){
+    const facts = document.querySelector(".facts");
+    const calories = facts.querySelector(".calories span:nth-child(2)");
+    const sugar = facts.querySelector(".sugar span:nth-child(2)");
+    const sodium = facts.querySelector(".sodium span:nth-child(2)");
+    const carb = facts.querySelector(".carbs span:nth-child(2)");
+
+    calories.textContent = `${fact?.nv?.calories} %`;
+    sugar.textContent = `${fact?.nv?.sugar} %`;
+    sodium.textContent = `${fact?.nv?.sodium} %`;
+    carb.textContent = `${fact?.nv?.carb} %`;
+
+    console.log({calories,sugar, sodium, carb});
+}
+
+// loop all the slot keys and get object 
 for (const key of slotKeys){
     const slotKey = key.textContent;
     key.addEventListener("click",function(){
+        fillNv(inventoryList.get(slotKey));
         infoElement.textContent = inventoryList.get(slotKey)?.name
+        handle(inventoryList.get(slotKey)?.price,billInserted.valueAsNumber);
+        console.dir(billInserted);
     })
 }
 
+function isFinite(num) { // NaN
+    // number is both less than 
+    return !(num > 0 == false && num < 0 == false)
+}
+// calculate function - needs to be improved 
+function handle(itemPrice,billInsert){
+    console.log(itemPrice,billInsert);
+    console.log(billInsert == NaN ,itemPrice == NaN)
+    
+    const balance = calculate(itemPrice,billInsert);
+    const isPostive = balance > 0;
+
+    if(!isFinite(itemPrice) || !isFinite(billInsert))
+        return console.log("Please Enter a valid number!")
+
+    if (isPostive) 
+        // item quantity --;
+        return console.log("Purchase successfull","Here is your change: ",balance);
+    else
+        return console.log("Insuffcient fund!","Please insert enough money, this much: " ,-balance)
+}
+
+// Pure function -  no side effects 
+function calculate(itemPrice,billInsert){
+    return billInsert-itemPrice;
+}
+
+// test if user put money
+// test if money enough
+// test if give the product
+// test if 
+
+// if typeof billInsert != Number
+//     then console.log(please insert a bill)
+
+// create varible balance = calculate();
+// create varible isPostive = balance >0
+// if isPostive
+//     alert(purchase successfull, balance)
+
+// else
+//     alert(insfuccient funds, balance)
