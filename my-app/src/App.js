8cc28eb7d8ecd4fb0467d7979537fb235a4db675
Yuan <gaoyuan0705@gmail.com>
2022-09-15 19:58:49 -0400

    html and css convert to react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,25 +3,63 @@ import './App.css';
 import Home, {Away} from "./Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Home />
-      </header>
+  return <>
+  <div class="wrapper">
+        <div class="nutrition-values">
+            <div class="title"> Nutrition Values</div>
+            <div class="facts">
+                <div class="row">
+                     <span>Amt/Serving</span>
+                     <span>% of daily</span>
+                </div>
+                <div class="row calories">
+                    <span>Calories</span>
+                    <span> %</span>
+               </div>
+               <div class="row sugar">
+                <span>Sugar</span>
+                <span>%</span>
+               </div>
+               <div class="row sodium">
+                <span>Sodium</span>
+                <span>%</span>
+               </div>
+               <div class="row carbs">
+                <span>Carbohydrates</span>
+                <span>%</span>
+               </div>
+                
+            </div>
+        </div>
+        <div class="keypad">
+            <div class="info">
+                Name of snack
+            </div>
+            <div class="slot-keys">
+                <span>A1</span>
+                <span>B1</span>
+                <span>C1</span>
+                <span>D1</span>
+                <span>A2</span>
+                <span>B2</span>
+                <span>C2</span>
+                <span>D2</span>
+                <span>A3</span>
+                <span>B3</span>
+                <span>C3</span>
+                <span>D3</span>
+                <span>A4</span>
+                <span>B4</span>
+                <span>C4</span>
+                <span>D4</span>
+            </div>
+            <div class="input">
+                <span> Insert Cash</span>
+                <input placeholder="$ 10.95" type="number"/>
+            </div>
+        </div>
     </div>
-  );
+  </>;
 }
 
 
