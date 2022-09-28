cf1c9d0602161b9c5dc62870532b02eb37d5535b
Yuan <gaoyuan0705@gmail.com>
2022-09-28 19:25:29 -0400

    useing state component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,209 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home, {Away} from "./Home";
 
 function App() {
+    const [inventoryList, updateInventoryList] = useState(new Map([
+        ["A1",{
+            name: "Coke",
+            price:3.5,
+            amount:6,
+            nv:{
+                calories:10,
+                sugar:5,
+                carb:2,
+                sodium:13,
+            }
+        }],
+        ["B1",{
+            name: "Chip",
+            price:4.5,
+            amount:6,
+            nv:{
+                calories:7,
+                sugar:4,
+                carb:8,
+                sodium:2,
+            }
+        }],
+        ["C1",{
+            name: "Donut",
+            price:5.5,
+            amount:6,
+            nv:{
+                calories:28,
+                sugar:1,
+                carb:9,
+                sodium:5,
+            }
+        }],
+        ["D1",{
+            name: "Monster",
+            price:6.5,
+            amount:6,
+            nv:{
+                calories:38,
+                sugar:7,
+                carb:6,
+                sodium:3,
+            }
+        }],
+    
+        ["A2",{
+            name: "Milk",
+            price:1.5,
+            amount:6,
+            nv:{
+                calories:10,
+                sugar:5,
+                carb:0,
+                sodium:0,
+            }
+        }],
+        ["B2",{
+            name: "Chocolate Milk",
+            price:4,
+            amount:6,
+            nv:{
+                calories:110,
+                sugar:21,
+                carb:62,
+                sodium:11,
+            }
+        }],
+        ["C2",{
+            name: "Timibits",
+            price:1,
+            amount:6,
+            nv:{
+                calories:10,
+                sugar:5,
+                carb:3,
+                sodium:2.5,
+            }
+        }],
+        ["D2",{
+            name: "Sprite",
+            price:3,
+            amount:6,
+            nv:{
+                calories:210,
+                sugar:54,
+                carb:20,
+                sodium:10,
+            }
+        }],
+    
+        ["A3",{
+            name: "Fanta",
+            price:4,
+            amount:6,
+            nv:{
+                calories:140,
+                sugar:18,
+                carb:9,
+                sodium:7,
+            }
+        }],
+        ["B3",{
+            name: "Pepsi",
+            price:6,
+            amount:6,
+            nv:{
+                calories:130,
+                sugar:5,
+                carb:8,
+                sodium:7,
+            }
+        }],
+        ["C3",{
+            name: "Skittles",
+            price:3.9,
+            amount:6,
+            nv:{
+                calories:16,
+                sugar:15,
+                carb:17,
+                sodium:18,
+            }
+        }],
+        ["D3",{
+            name: "Candy",
+            price:1.5,
+            amount:6,
+            nv:{
+                calories:14,
+                sugar:25,
+                carb:20,
+                sodium:20,
+            }
+        }],
+    
+        ["A4",{
+            name: "Protein bar",
+            price:5.5,
+            amount:6,
+            nv:{
+                calories:290,
+                sugar:13,
+                carb:19,
+                sodium:20,
+            }
+        }],
+        ["B4",{
+            name: "Coffee",
+            price:1,
+            amount:6,
+            nv:{
+                calories:0,
+                sugar:0,
+                carb:0,
+                sodium:0,
+            }
+        }],
+        ["C4",{
+            name: "Bubble Tea",
+            price:3.6,
+            amount:6,
+            nv:{
+                calories:18,
+                sugar:17,
+                carb:98,
+                sodium:99,
+            }
+        }],
+        ["D4",{
+            name: "Apple Pie",
+            price:6.7,
+            amount:6,
+            nv:{
+                calories:37,
+                sugar:45,
+                carb:99,
+                sodium:36,
+            }
+        }],
+    ]));
+    console.log({inventoryList,updateInventoryList, amount: inventoryList.get('A1')});
+    let myOtherNumber = 0;
+    setTimeout(function(){
+        inventoryList.set('A1', {
+            name: "Coke",
+            price:4.5,
+            amount:3,
+            nv:{
+                calories:10,
+                sugar:5,
+                carb:2,
+                sodium:13,
+            }
+        })
+        updateInventoryList(inventoryList);
+        console.log({inventoryList,updateInventoryList, amount: inventoryList.get('A1')});
+    }, 3000)
   return <>
+  {JSON.stringify(inventoryList.get('A1'))}
   <div class="wrapper">
         <div class="nutrition-values">
             <div class="title"> Nutrition Values</div>
